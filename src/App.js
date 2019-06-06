15c5d7471bb5881203984964967b4d52ad947605
WuBryant <dream52095@gmail.com>
2019-06-06 16:04:21 +0800

    Set  semantic-ui-react.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react'
 
 function App() {
   return (
     <div>
+      <Button primary >Semantic-ui-react Success!!</Button>
     </div>
   );
 }
